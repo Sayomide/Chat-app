094b50595fd33747432e2cbac1cf8ed2d362420d
ayomide <mikaheelayomide5@gmail.com>
2024-01-18 09:06:42 +0100

    done with html button

---
+++ chat.js
@@ -0,0 +1,29 @@
+/*
+document.addEventListener("DOMContentLoaded", () => {
+  let groupChat = document.querySelector("#group");
+  let indivChat = document.querySelector("#friend");
+
+  groupChat.addEventListener("click", () => {
+    console.log("clicked");
+  });
+});
+*/
+let groupChat = document.querySelector("#group");
+let indivChat = document.querySelector("#friend");
+
+groupChat.addEventListener("click", () => {
+  
+  
+  
+  console.log("For group chat")
+});
+
+
+
+
+
+
+
+indivChat.addEventListener("click", () => {
+  console.log("For individual chat")
+})
